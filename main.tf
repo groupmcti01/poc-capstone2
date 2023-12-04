0342f33cbf6fde53a924975f897d86abe96d9f54
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-04 18:09:59 -0500

    add workflow and scheduler

--- main.tf
+++ main.tf
@@ -31,3 +31,38 @@ resource "google_storage_bucket" "capstone2_poc" {
     }
 
 }
+
+data "google_compute_default_service_account" "default" {
+  project = var.project_id
+}
+
+resource "google_cloud_scheduler_job" "mcit-capstone2-scheduler-workflow" {
+  project           =   var.project_id
+  name              =   ""
+  description       =   ""
+  schedule          =   ""
+  time_zone         =   ""
+  attempt_deadline  =   ""
+  region            =   ""
+
+
+  http_target {
+    http_method     =   "POST"
+    uri             =   "https://workflowexecutions.googleapis.com/v1/${google_workflows_workflow.mcti-capstone2-workflow.id}/executions"
+
+  oauth_token {
+    service_account_email   = ""
+    scope                   = ""
+  }
+  }
+}
+
+resource "google_workflows_workflow" "workflow" {
+  name            = ""
+  region          = ""
+  description     = ""
+  service_account = ""
+  project         = ""
+  labels          = ""
+  source_contents = ""
+}
