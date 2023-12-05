7e7f872ca6cd8cbebbd0a303ebd7f97b2e93b0d6
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-04 22:56:35 -0500

    templatefile

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "google_workflows_workflow" "mcti-capstone2-workflow-poc" {
     env = "poc"
   }
   project         = var.project_id
-  source_contents = templatefile("${path.root}/templates/export.yaml",{ project = var.projectid })
+  source_contents = templatefile("${path.root}/templates/export.yaml",{ project = var.project_id })
 }
 
 resource "time_sleep" "wait_60_seconds" {
