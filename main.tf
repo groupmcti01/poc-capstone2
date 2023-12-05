46e9698aa525ef34debd80294e999ea70c0bda53
mcti2dcoronado <dcoronado.mcti@gmail.com>
2023-12-04 20:54:40 -0500

    fix templatefile function

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "google_workflows_workflow" "mcti-capstone2-workflow-poc" {
     env = "poc"
   }
   project         = var.project_id
-  #source_contents = templatefile(yamls/export.yaml,{})
+  source_contents = templatefile(yamls/export.yaml,{})
 }
 
 resource "time_sleep" "wait_60_seconds" {
